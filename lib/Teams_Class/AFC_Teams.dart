d75faceead85c15e637be385f0636550a4b44eb7
Bronziel Gold <morgangiermogrimes@gmail.com>
2023-11-16 16:42:31 +0100

    basic

---
+++ lib/Teams_Class/AFC_Teams.dart
@@ -0,0 +1,15 @@
+class Ravens {
+  static const String name = 'Ravens';
+  static const int id = 1;
+  static const String mobileImagePath =
+      'assets/mobileteam/AFC/AFC_North/ravensMobile.jpg';
+  static const String imagePath = 'assets/Team/AFC/AFC_North/ravens-01.jpg';
+}
+
+class Bengals {
+  static const String name = 'Bengals';
+  static const int id = 2;
+  static const String mobileImagePath =
+      'assets/Team/AFC/AFC_North/bengalsMobile.jpg';
+  static const String imagePath = 'assets/Team/AFC/AFC_North/bengals-01.jpg';
+}
--- lib/team_selector.dart
+++ lib/team_selector.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'team_data.dart';
 
 void main() => runApp(MyApp());
 
