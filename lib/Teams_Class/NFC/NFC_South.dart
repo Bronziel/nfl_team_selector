e0d2f97cb35d417fb3f374266d519e706019208a
Bronziel Gold <morgangiermogrimes@gmail.com>
2023-11-17 23:46:43 +0100

    all nfc teams

---
+++ lib/Teams_Class/NFC/NFC_South.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class Saints {
+  static const String name = 'New Orleans Saints';
+  static const int id = 13;
+  static const String normalImagePath =
+      'web/assets/Team/NFC/NFC_South/saints.jpg';
+  static const String winImagePath =
+      'web/assets/winner/NFC/NFC_South/saints-01.jpg';
+
+  static const Color mainColor = Color(0xFFD3BC8D); //gold
+  static const List<Color> colors = [
+    Color(0xFF101820), //black
+  ];
+  static const String division = 'NFC South';
+  static const String conference = 'NFC';
+  static const String city = 'New Orleans';
+  static const String state = 'Louisiana';
+}
+
+class Buccaneers {
+  static const String name = 'Tampa Bay Buccaneers';
+  static const int id = 14;
+  static const String normalImagePath =
+      'web/assets/Team/NFC/NFC_South/buccaneers.jpg';
+  static const String winImagePath =
+      'web/assets/winner/NFC/NFC_South/buccaneers-01.jpg';
+
+  static const Color mainColor = Color(0xFFD50A0A); //red
+  static const List<Color> colors = [
+    Color(0xFFFF7900), //bay orange
+    Color(0xFF0A0A08), //black
+    Color(0xFFB1BABF), //grey
+    Color(0xFF34302B), //pewter
+  ];
+  static const String division = 'NFC South';
+  static const String conference = 'NFC';
+  static const String city = 'Tampa Bay';
+  static const String state = 'Florida';
+}
+
+class Falcons {
+  static const String name = 'Atlanta Falcons';
+  static const int id = 15;
+  static const String normalImagePath =
+      'web/assets/Team/NFC/NFC_South/falcons.jpg';
+  static const String winImagePath =
+      'web/assets/winner/NFC/NFC_South/falcons-01.jpg';
+
+  static const Color mainColor = Color(0xFFA71930); //red
+  static const List<Color> colors = [
+    Color(0xFF000000), //black
+    Color(0xFFA5ACAF), //silver
+  ];
+  static const String division = 'NFC South';
+  static const String conference = 'NFC';
+  static const String city = 'Atlanta';
+  static const String state = 'Georgia';
+}
+
+class Panthers {
+  static const String name = 'Carolina Panthers';
+  static const int id = 16;
+  static const String normalImagePath =
+      'web/assets/Team/NFC/NFC_South/panthers.jpg';
+  static const String winImagePath =
+      'web/assets/winner/NFC/NFC_South/panthers-01.jpg';
+
+  static const Color mainColor = Color(0xFF0085CA); //Carolina Blue
+  static const List<Color> colors = [
+    Color(0xFF101820), //black
+    Color(0xFFBFC0BF), //silver
+  ];
+  static const String division = 'NFC South';
+  static const String conference = 'NFC';
+  static const String city = 'Charlotte';
+  static const String state = 'North Carolina';
+}
---
+++ lib/Teams_Class/NFC/NFC_West.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+class SF49ers {
+  static const String name = 'San Francisco 49ers';
+  static const int id = 5;
+  static const String normalImagePath =
+      'web/assets/Team/NFC/NFC_West/49ers.jpg';
+  static const String winImagePath =
+      'web/assets/winner/NFC/NFC_West/49ers-01.jpg';
+
+  static const Color mainColor = Color(0xFFAA0000); //red
+  static const List<Color> colors = [
+    Color(0xFFB3995D), //gold
+  ];
+  static const String division = 'NFC West';
+  static const String conference = 'NFC';
+  static const String city = 'San Francisco';
+  static const String state = 'California';
+}
+
+class SeaHawks {
+  static const String name = 'Seattle Seahawks';
+  static const int id = 6;
+  static const String normalImagePath =
+      'web/assets/Team/NFC/NFC_West/seahawks.jpg';
+  static const String winImagePath =
+      'web/assets/winner/NFC/NFC_West/seahawks-01.jpg';
+
+  static const Color mainColor = Color(0xFF002244); //blu dark
+  static const List<Color> colors = [
+    Color(0xFF69BE28), //green
+    Color(0xFFA5ACAF), //grey
+  ];
+  static const String division = 'NFC West';
+  static const String conference = 'NFC';
+  static const String city = 'Seattle';
+  static const String state = 'Washington';
+}
+
+class Rams {
+  static const String name = 'Los Angeles Rams';
+  static const int id = 7;
+  static const String normalImagePath = 'web/assets/Team/NFC/NFC_West/rams.jpg';
+  static const String winImagePath =
+      'web/assets/winner/NFC/NFC_West/rams-01.jpg';
+
+  static const Color mainColor = Color(0xFF003594); //blu
+  static const List<Color> colors = [
+    Color(0xFFFFA300), //gold
+    Color(0xFFFF8200), //Dark gold
+    Color(0xFFFFD100), //yellow
+    Color(0xFFFFFFFF), //white
+  ];
+  static const String division = 'NFC West';
+  static const String conference = 'NFC';
+  static const String city = 'Los Angeles';
+  static const String state = 'California';
+}
+
+class Cardinals {
+  static const String name = 'Arizona Cardinals';
+  static const int id = 8;
+  static const String normalImagePath =
+      'web/assets/Team/NFC/NFC_West/cardinals.jpg';
+  static const String winImagePath =
+      'web/assets/winner/NFC/NFC_West/cardinals-01.jpg';
+
+  static const Color mainColor = Color(0xFF97233F); //red
+  static const List<Color> colors = [
+    Color(0xFF000000), //black
+    Color(0xFFFFB612), //yellow
+  ];
+  static const String division = 'NFC West';
+  static const String conference = 'NFC';
+  static const String city = 'Phoenix';
+  static const String state = 'Arizona';
+}
