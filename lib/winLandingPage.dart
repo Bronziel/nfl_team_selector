c92ff9c6ea697138c30f28d80e00cc9b6a0b9d3f
Bronziel Gold <morgangiermogrimes@gmail.com>
2023-11-28 11:45:40 +0100

    aded it new page

---
+++ lib/winLandingPage.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'Teams_Class/ClassTeam.dart';
+
+class WinnerPage extends StatelessWidget {
+  final Team winner;
+
+  const WinnerPage({Key? key, required this.winner}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Winner'),
+      ),
+      body: SingleChildScrollView(
+        // Wrap content in a SingleChildScrollView
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Congratulations to ${winner.name}!',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 20),
+              Image.asset(
+                winner.winImagePath,
+                fit: BoxFit.contain,
+              ),
+              SizedBox(height: 20),
+              FilledButton(
+                onPressed: () {
+                  Navigator.of(context).pop(); // Go back to the previous screen
+                },
+                child: Text('Back to Selection'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
